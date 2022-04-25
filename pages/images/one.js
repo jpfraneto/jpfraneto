efe86d5a6cf3a2db7b5bbdfa036ab71e9e6c08f2
jpfraneto <jpfraneto@gmail.com>
2022-04-25 18:24:42 -0400

    new image

---
+++ pages/images/one.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+const One = () => {
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <img src={'/images/jps/1.png'} height='100%' width='100%' />
+    </div>
+  );
+};
+
+export default One;
