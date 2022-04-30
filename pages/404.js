9cd191aa37c030520b560affb20b42a981983222
jpfraneto <jpfraneto@gmail.com>
2022-04-30 18:07:45 -0400

    added 404

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+  return (
+    <p style={{ color: 'white' }}>
+      Oooops, this page doesn't exist yet. If you think it would be great to
+      have it here, write me an email to jpfraneto@gmail.com and I'll add it.
+      Thank you for your trust.
+    </p>
+  );
+}
